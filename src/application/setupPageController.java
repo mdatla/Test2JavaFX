bc84eba7202c0d51bab69a43e4881178a89c6804
mdatla <mdatla@cse.unl.edu>
2016-03-27 15:52:50 -0500

    final commit

--- src/application/setupPageController.java
+++ src/application/setupPageController.java
@@ -8,9 +8,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class setupPageController implements Initializable{
@@ -18,14 +21,16 @@ public class setupPageController implements Initializable{
 	@FXML static ListView<String> classListView = new ListView<String>();
 	
 	@FXML protected void startButtonPressed(ActionEvent event) throws IOException{
-		ObservableList<String> classSource = FXCollections.observableArrayList(
-				"CSCE 330", "CSCE 372", "CSCE 451");
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("timerBlueRoll.fxml"));
+		
+		VBox vbox = (VBox) loader.load();
+		Scene scene = new Scene (vbox);
 		Node source = (Node) event.getSource();
 		Stage stage = (Stage) source.getScene().getWindow();
-		ListView list = (ListView)stage.getScene().lookup("#classListView");
-		list.setItems(classSource);
+		stage.setScene(scene);
+		stage.show();
 	}
-
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
